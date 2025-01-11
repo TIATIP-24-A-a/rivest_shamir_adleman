c691b04d618686c9d19033723c9ef34c95b18495
CodingNerd187 <unterguggenberger.david@gmail.com>
2025-01-11 16:00:39 +0100

    Added prime utils header declaration

---
+++ src/prime_utils.h
@@ -0,0 +1,17 @@
+#ifndef PRIME_UTILS_H_
+#define PRIME_UTILS_H_
+
+namespace PrimeUtils {
+
+    // Determines if a given number is prime.
+    //
+    // Args:
+    //   number: The integer to check.
+    //
+    // Returns:
+    //   True if the number is prime, false otherwise.
+    bool IsPrime(int number);
+
+}  // namespace PrimeUtils
+
+#endif  // PRIME_UTILS_H_
