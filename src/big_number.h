731f36a00c572bf347e1a93b02b66da9a6278286
CodingNerd187 <unterguggenberger.david@gmail.com>
2025-01-11 23:41:12 +0100

    Add empty big number header

---
+++ src/big_number.h
@@ -0,0 +1,14 @@
+//
+// Created by David on 11.01.2025.
+//
+
+#ifndef RIVEST_SHAMIR_ADLEMAN_BIG_NUMBER_H
+#define RIVEST_SHAMIR_ADLEMAN_BIG_NUMBER_H
+
+
+class big_number {
+
+};
+
+
+#endif //RIVEST_SHAMIR_ADLEMAN_BIG_NUMBER_H
