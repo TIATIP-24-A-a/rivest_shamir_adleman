536cd47bb31859761e776279b8e8edec1369c07e
CodingNerd187 <unterguggenberger.david@gmail.com>
2025-01-11 16:34:22 +0100

    added header for generate prime numbers, and stick to google style code rules

--- src/prime_utils.h
+++ src/prime_utils.h
@@ -1,17 +1,29 @@
 #ifndef PRIME_UTILS_H_
 #define PRIME_UTILS_H_
 
+#include <random>
+
 namespace PrimeUtils {
 
-    // Determines if a given number is prime.
-    //
-    // Args:
-    //   number: The integer to check.
-    //
-    // Returns:
-    //   True if the number is prime, false otherwise.
+    /* Checks whether a given number is prime.
+     * Args:
+     *   number: The number to check.
+     * Returns:
+     *   True if the number is prime, otherwise false.
+     */
     bool IsPrime(int number);
 
-}  // namespace PrimeUtils
+    /* Generates a random prime number within the specified range.
+     * Args:
+     *   min: The minimum value of the range (inclusive).
+     *   max: The maximum value of the range (inclusive).
+     * Returns:
+     *   A random prime number within the range [min, max].
+     * Throws:
+     *   std::invalid_argument if the range is invalid.
+     */
+    int GeneratePrime(int min, int max);
+
+}
 
 #endif  // PRIME_UTILS_H_
