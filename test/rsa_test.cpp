2989392118d49928e980198c168a56c7967a6b39
CodingNerd187 <unterguggenberger.david@gmail.com>
2025-01-11 15:59:49 +0100

    Add failing test for PrimeUtils::IsPrime function.

--- test/rsa_test.cpp
+++ test/rsa_test.cpp
@@ -1,3 +1,19 @@
-//
-// Created by David on 11.01.2025.
-//
+#include "../src/prime_utils.h"
+#include <cassert>
+#include <iostream>
+
+// Tests the IsPrime function in PrimeUtils.
+void TestIsPrime() {
+    // Test a known prime number.
+    assert(PrimeUtils::IsPrime(17) == true);
+
+    // Test a known non-prime number.
+    assert(PrimeUtils::IsPrime(18) == false);
+
+    std::cout << "TestIsPrime passed!" << std::endl;
+}
+
+int main() {
+    TestIsPrime();  // Run the IsPrime test.
+    return 0;
+}
