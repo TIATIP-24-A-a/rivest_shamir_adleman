d237eb6c3b79c782b71f87ce67dc2b59b5a42d55
Stevic-Nenad <stevic.nenad@icloud.com>
2025-01-13 00:03:55 +0100

    added methods to header

--- src/prime_utils.h
+++ src/prime_utils.h
@@ -8,6 +8,8 @@ namespace PrimeUtils {
     bool IsPrime(const BigNumber& number);
     BigNumber GeneratePrime(const BigNumber& min, const BigNumber& max);
     BigNumber GeneratePrimeWithBitLength(int bitLength);
+    BigNumber GenerateRSASafePrime(int bitLength);
+    bool IsRSASafe(const BigNumber& prime);
 }
 
 #endif  // PRIME_UTILS_H_
