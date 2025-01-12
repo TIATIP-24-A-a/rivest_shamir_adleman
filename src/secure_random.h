d4a5bf2dbbcaaf20dfd130638d5b12ef8320c254
Stevic-Nenad <stevic.nenad@icloud.com>
2025-01-13 00:06:51 +0100

    updated header of secure random to now use big numbers instead of ints

--- src/secure_random.h
+++ src/secure_random.h
@@ -1,5 +1,6 @@
 // secure_random.h
 #pragma once
+#include <big_number.h>
 #include <cstdint>
 #include <stdexcept>
 
@@ -9,5 +10,5 @@ public:
     void GetBytes(uint8_t* buffer, size_t size);
 
     // Get random integer in range [min, max]
-    uint64_t GetRange(uint64_t min, uint64_t max);
+    BigNumber GetBigNumberRange(const BigNumber& min, const BigNumber& max);
 };
