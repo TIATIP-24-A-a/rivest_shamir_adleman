afff10e8c1a6141a352d1fe3022890937e3d32c8
Stevic-Nenad <stevic.nenad@icloud.com>
2025-01-12 23:38:03 +0100

    refactored prime utils to now use big numbers instead of ints

--- src/prime_utils.h
+++ src/prime_utils.h
@@ -1,28 +1,14 @@
 #ifndef PRIME_UTILS_H_
 #define PRIME_UTILS_H_
 
+#include <big_number.h>
 #include <random>
 
 namespace PrimeUtils {
 
-    /* Checks whether a given number is prime.
-     * Args:
-     *   number: The number to check.
-     * Returns:
-     *   True if the number is prime, otherwise false.
-     */
-    bool IsPrime(int number);
+    bool IsPrime(const BigNumber& number, int rounds = 64);
 
-    /* Generates a random prime number within the specified range.
-     * Args:
-     *   min: The minimum value of the range (inclusive).
-     *   max: The maximum value of the range (inclusive).
-     * Returns:
-     *   A random prime number within the range [min, max].
-     * Throws:
-     *   std::invalid_argument if the range is invalid.
-     */
-    int GeneratePrime(int min, int max);
+    BigNumber GeneratePrime(const BigNumber& min, const BigNumber& max);
 
 }
 
