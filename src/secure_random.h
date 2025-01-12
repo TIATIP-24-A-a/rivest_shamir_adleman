a9187cc25fe9dc1007d484c7a106216ea04dcff3
Stevic-Nenad <stevic.nenad@icloud.com>
2025-01-13 00:08:13 +0100

    refactored header

--- src/secure_random.h
+++ src/secure_random.h
@@ -6,13 +6,12 @@
 
 class SecureRandom {
 public:
+    // Get random integer in range [min, max]
     uint64_t GetRange(uint64_t min, uint64_t max);
 
-    // Get random bytes into the provided buffer
-    void GetBytes(uint8_t* buffer, size_t size);
-
-    // Get random integer in range [min, max]
+    // Get random Big Number in range
     BigNumber GetBigNumberRange(const BigNumber& min, const BigNumber& max);
 private:
-
+    // Get random bytes into the provided buffer
+    void GetBytes(uint8_t* buffer, size_t size);
 };
