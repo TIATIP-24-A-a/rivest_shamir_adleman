4281404e90c97f49ba6c840999277f0205d3fc72
Stevic-Nenad <stevic.nenad@icloud.com>
2025-01-13 00:07:42 +0100

    readded old int version for backwards compatibility

--- src/secure_random.h
+++ src/secure_random.h
@@ -6,9 +6,13 @@
 
 class SecureRandom {
 public:
+    uint64_t GetRange(uint64_t min, uint64_t max);
+
     // Get random bytes into the provided buffer
     void GetBytes(uint8_t* buffer, size_t size);
 
     // Get random integer in range [min, max]
     BigNumber GetBigNumberRange(const BigNumber& min, const BigNumber& max);
+private:
+
 };
