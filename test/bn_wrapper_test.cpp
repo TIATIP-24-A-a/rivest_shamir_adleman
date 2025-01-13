b89c197539a06fcd3fe9eb1daa28340d6cb5688e
Stevic-Nenad <stevic.nenad@icloud.com>
2025-01-13 12:13:02 +0100

    added new test to check ptr value

--- test/bn_wrapper_test.cpp
+++ test/bn_wrapper_test.cpp
@@ -1,6 +1,7 @@
 #include "../src/bn_wrapper.h"
 #include <iostream>
 #include <cassert>
+#include <openssl/bn.h>
 
 void TestBNPtrBasicCreation() {
     try {
@@ -11,8 +12,20 @@ void TestBNPtrBasicCreation() {
         std::cerr << "TestBNPtrBasicCreation failed with exception: " << e.what() << std::endl;
     }
 }
+void TestBNPtrValue() {
+    try {
+        BN_ptr num;
+        BN_set_word(num.get(), 123);
+        assert(BN_get_word(num.get()) == 123);
+        std::cout << "TestBNPtrValue passed!" << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "TestBNPtrValue failed with exception: " << e.what() << std::endl;
+    }
+}
 
+// Update main():
 int main() {
     TestBNPtrBasicCreation();
+    TestBNPtrValue();
     return 0;
 }
