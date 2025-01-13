b07db7f1f272c934b78c50eab5889a31c9f43988
Stevic-Nenad <stevic.nenad@icloud.com>
2025-01-13 12:06:30 +0100

    refactored wrapper class for consistency across project

--- src/bn_wrapper.h
+++ src/bn_wrapper.h
@@ -1,20 +1,14 @@
 // include/bn_wrapper.h
 #pragma once
 #include <openssl/bn.h>
-#include <stdexcept>
 
 class BN_ptr {
 public:
-    BN_ptr() : bn(BN_new()) {
-        if (!bn) throw std::runtime_error("BN_new failed");
-    }
+    BN_ptr();
+    ~BN_ptr();
 
-    ~BN_ptr() {
-        if (bn) BN_free(bn);
-    }
-
-    BIGNUM* get() { return bn; }
-    const BIGNUM* get() const { return bn; }
+    BIGNUM* get();
+    const BIGNUM* get() const;
 
 private:
     BIGNUM* bn;
